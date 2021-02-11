56e581f3e88efb688a3125db395063e3120fa326
konsin1988 <konsin1988@gmail.com>
2021-02-11 14:58:25 +0300

    MKM_FUNCTION

---
+++ MKM_COMMANDS/README.txt
@@ -0,0 +1,11 @@
+/* Command for linux terminal of bash shell. 
+/* Performs (in the presence of the "Makefile" file 
+/* and the "clean" command in it) sequentially 
+/* first cleaning the directory from .o files, 
+/* then clears the terminal window, 
+/* then builds the program (if there is a Makefile). 
+/* The "c" argument only allows you to clean the terminal 
+/* window ("mkm c"), and if you supply the name of 
+/* an executable program as an argument, it starts 
+/* its execution ("mkm name", where name is the 
+/* name of the program).
---
+++ MKM_COMMANDS/main.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	if(argc<=1)
+	{
+		system("make clean");
+		system("clear");
+		system("make");
+	}
+	else if(argc == 2)
+	{
+		if(!strcmp(argv[1], "c"))
+		{
+			system("clear");
+		}
+		else
+		{
+			char command[20] = "./";
+			strcat(command, argv[1]);
+			system("make clean");
+			system("clear");
+			system("make");
+			printf("\n\t\t+++FUNCTION <%s> "
+				"IN WORK+++\n\n", argv[1]);
+			system(command);
+			printf("\n\n");
+		}
+	}
+	return 0;
+}
